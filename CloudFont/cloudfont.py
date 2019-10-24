e030826b3bbe2dad19b471d2d4474b7cb20f66d3
xuchengzhi <xudear@live.com>
2019-10-24 20:45:51 +0800

    生成云次

--- CloudFont/cloudfont.py
+++ CloudFont/cloudfont.py
@@ -9,9 +9,20 @@ from wordcloud import WordCloud,ImageColorGenerator
  
 TList = ["我与父亲不相见已二年余了我最不能忘记的是他的背影那年冬天祖母死了父亲的差使也交卸了正是祸不单行的日子我从北京到徐州打算跟着父亲奔丧回家到徐州见着父亲看见满院狼藉的东西又想起祖母不禁簌簌地流下眼泪父亲说事已如此不必难过好在天无绝人之路回家变卖典质父亲还了亏空又借钱办了丧事这些日子家中光景很是惨澹一半为了丧事一半为了父亲赋闲丧事完毕父亲要到南京谋事我也要回北京念书我们便同行到南京时有朋友约去游逛勾留了一日第二日上午便须渡江到浦口下午上车北去父亲因为事忙本已说定不送我叫旅馆里一个熟识的茶房陪我同去他再三嘱咐茶房甚是仔细但他终于不放心怕茶房不妥帖颇踌躇了一会其实我那年已二十岁北京已来往过"]
  
+
+with open('content.txt','r',encoding='utf-8') as f:
+    word= (f.read())
+    f.close()
+
+resultword=re.sub("[A-Za-z0-9\[\`\~\!\@\#\$\^\&\*\(\)\=\|\{\}\'\:\;\'\,\[\]\.\<\>\/\?\~\。\@\#\\\&\*\%]", "",word) 
+
+wordlist_after_jieba = jieba.cut(resultword) 
+wl_space_split = " ".join(wordlist_after_jieba)
+
+
 def cloud_pic():
-    raw_signature_string = ''.join(TList)
-    text = jieba.cut(raw_signature_string, cut_all=True)
+    # raw_signature_string  = " ".join(wordlist_after_jieba)
+    text = jieba.cut(resultword, cut_all=True)
     wl_space_split = ' '.join(text)
  
     d = os.path.dirname(os.path.abspath(__file__))
